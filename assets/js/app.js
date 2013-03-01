a08e188d292f21f88a0eac3fcbb33abedec89a76
sara <speerbho@iupui.edu>
2013-03-01 14:57:05 -0500

    testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,43 @@
 ;(function($, window) {
-	$(document).ready(function(){
 	
-		$('.toggle').click(function(e) {
-			var $t = $(this);
-			var id = $t.attr('href');
-			var target = $(id);
-			
-			console.log(id);
-			
-			if(target.css('display') != 'none') {
-				target.removeClass('show');
-			}
-			else {
-				target.addClass('show');
-			}
-			
-			e.preventDefault();
-		});
+    // Some sample Javascript functions:
+    $(function(){
 	
-		$('.bars').click(function(e){
-			
-			var $t = $(this);
-			var $nav = $('.nav');
-			
-			if($t.hasClass('rotate')){
-				$t.removeClass('rotate');
-				$nav.hide();
-			}
-				else{
-				$t.addClass('rotate');
-				$nav.show();
-			}
-			e.preventDefault();
-		});
-	
+	    var jQT = new $.jQTouch({
+	        statusBar: 'black-translucent',
+	        preloadImages: []
+	    });
+
+    });
+    
+	$('.toggle').click(function(e) {
+		var $t = $(this);
+		var id = $t.attr('href');
+		var target = $(id);
+		
+		console.log(id);
+		
+		if(target.css('display') != 'none') {
+			target.removeClass('show');
+		}
+		else {
+			target.addClass('show');
+		}
+		
+		e.preventDefault();
 	});
+	
+	$('.bars').click(function(e) {
+		var $t = $(this);
+		
+		if($t.hasClass('rotate')) {
+			$t.removeClass('rotate');
+		}
+		else {
+			$t.addClass('rotate');
+		}
+				
+		e.preventDefault();
+	});
+	
 }(jQuery, this));
